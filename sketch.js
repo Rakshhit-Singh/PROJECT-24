c30317c8b2e5c37e4381f15ac86b0cf7d676a6ed
Rakshhit-Singh <72254094+Rakshhit-Singh@users.noreply.github.com>
2020-11-03 22:08:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,59 +1,39 @@
+const Engine = Matter.Engine; 
+const World = Matter.World; 
+const Bodies = Matter.Bodies; 
+const Body = Matter.Body; 
+var side1,side1,bottom,ball,ground,dustbinimage; 
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+function preload() { 
 
-var paperObject, ground;
-var dustbin1, dustbin2, dustbin3;
+} 
 
-function preload()
-{
-	
+function setup() { 
+createCanvas(800, 700); 
+engine = Engine.create(); 
+world = engine.world; 
+//Create the Bodies Here. 
+side1= new Dustbin(425,615,20,150) 
+side2= new Dustbin(625,615,20,150) 
+bottom= new Dustbin(525,675,200,20) 
+ground= new Dustbin(400,690,800,10) 
+ball = new Paper(100,50,50) 
+Engine.run(engine); 
+
+} 
+
+function draw() { 
+rectMode(CENTER); 
+background("black"); 
+side1.display();
+side2.display();
+ground.display();
+bottom.display(); 
+ball.display(); 
+} 
+
+function keyPressed(){ 
+    if (keyCode === UP_ARROW){ 
+        Matter.Body.applyForce(ball.body,ball.body.position,{x:45,y:-225}) 
+    } 
 }
-
-function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-
-	engine = Engine.create();
-	world = engine.world;
-
-	ground = new Ground(width/2,690,width,20);
-
-	paperObject = new Paper(60,590,55);
-
-	dustbin1=new Dustbin(700,630,20,100);
-	dustbin2=new Dustbin(500,630,20,100);
-	dustbin3=new Dustbin(600,670,200,20);	
-
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-    background(0);
-
-Engine.update(engine);
-
-  ground.display();
-  paperObject.display();
-  dustbin1.display();
-  dustbin2.display();
-  dustbin3.display();
-
-  keyPressed();
-  drawSprites();
- 
-}
-
-function keyPressed(){
-	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(paperObject.body, paperObject.body.position, {x:20,y:-97});
-		console.log(paperObject.body.position)
-	}
-
-}
-
-
